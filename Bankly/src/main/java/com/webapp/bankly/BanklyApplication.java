d6f9724e8c0ca6c51bf6ab63497ad2def04a8066
nikol4e2 <arsovnikola1@gmail.com>
2025-12-23 19:24:53 +0100

    initialised project

---
+++ Bankly/src/main/java/com/webapp/bankly/BanklyApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.bankly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanklyApplication.class, args);
+    }
+
+}
---
+++ Bankly/src/test/java/com/webapp/bankly/BanklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.bankly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
